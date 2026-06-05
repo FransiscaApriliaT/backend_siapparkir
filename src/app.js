6d90f29a7611d570fdb4b46d72e83e64dd8e129e
FransiscaApriliaT <fransiscaapriliatarabu@gmail.com>
2026-06-06 00:39:25 +0800

    lagii

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const errorHandler  = require('./middleware/errorHandler');
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // Sesuaikan dengan port Vite Anda (biasanya 5173)
+  origin: "https://frontend-siapparkir.vercel.app/", // Sesuaikan dengan port Vite Anda (biasanya 5173)
   credentials: true
 }));
 app.use(express.json());
